0414247ef7b697d2982ee18ff2087f94b313d571
AmaniEzz <amanymounas@gmail.com>
2021-03-11 00:39:15 +0300

    commit updates

--- commerce/settings.py
+++ commerce/settings.py
@@ -84,10 +84,26 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'Auctions',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 AUTH_USER_MODEL = 'auctions.User'
