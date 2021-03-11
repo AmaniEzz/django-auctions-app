74b46aed84c5ae76d95d5582b8de9c06a964add7
AmaniEzz <amanymounas@gmail.com>
2021-03-11 22:19:48 +0300

    commit updates

--- commerce/settings.py
+++ commerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +147,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
