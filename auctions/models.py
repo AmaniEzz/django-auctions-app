0004cd4f503c4f71965e7bdbb04f7334ee380efc
AmaniEzz <amanymounas@gmail.com>
2021-03-11 21:13:30 +0300

    commit updates

--- auctions/models.py
+++ auctions/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.urls import reverse
 
-
 class User(AbstractUser):
     pass
 
@@ -36,12 +35,12 @@ class Listings(models.Model):
     starting_bid  = models.DecimalField(max_digits=10, decimal_places=2)
     highest_bid   = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     desired_price = models.DecimalField(max_digits=10, decimal_places=2)
-    category     = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="category")
-    image        = models.ImageField(upload_to='images/')
-    created_at   = models.DateTimeField(auto_now_add=True)
-    Winner       = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, related_name="winner", null=True)
-    active       = models.BooleanField(default=True)
-    comment      = models.ManyToManyField(Comment, blank=True)
+    category      = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="category")
+    image         = models.ImageField(upload_to='images/')
+    created_at    = models.DateTimeField(auto_now_add=True)
+    Winner        = models.ForeignKey(User, on_delete=models.CASCADE,blank=True, related_name="winner", null=True)
+    active        = models.BooleanField(default=True)
+    comment       = models.ManyToManyField(Comment, blank=True)
 
 
     def __str__(self):
@@ -53,6 +52,7 @@ class Listings(models.Model):
 
 # Table bid, stores all info related to a bid
 class Bid(models.Model):
+    id          = models.AutoField(primary_key=True, serialize=False)
     bidder      = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bid_owner")
     listingid   = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="Bid_listing")
     bid_value   = models.DecimalField(max_digits=10, decimal_places=2)
--- commerce/settings.py
+++ commerce/settings.py
@@ -91,9 +91,9 @@ DATABASES = {
 
         'NAME': 'Auctions',
 
-        'USER': 'postgres',
+        'USER': 'myprojectuser',
 
-        'PASSWORD': '',
+        'PASSWORD': 'password',
 
         'HOST': 'localhost',
 
@@ -137,21 +137,21 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+# Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db  =  dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(prod_db)
