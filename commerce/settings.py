1727f78a616f9de2757410644122759df30b9a3e
AmaniEzz <amanymounas@gmail.com>
2021-03-11 21:46:29 +0300

    commit updates

--- commerce/settings.py
+++ commerce/settings.py
@@ -138,19 +138,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-PROJECT_ROOT   =  PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-#  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=600, ssl_require=True)
