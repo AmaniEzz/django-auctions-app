414f5800d8878af95ec9a0d789d08e967409914e
AmaniEzz <amanymounas@gmail.com>
2021-03-12 03:15:39 +0300

    commit updates

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn commerce.wsgi --log-file -
+
--- commerce/settings.py
+++ commerce/settings.py
@@ -26,7 +26,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['djangoauctionapp.herokuapp.com', '127.0.0.1']
 
